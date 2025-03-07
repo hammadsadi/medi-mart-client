73f2e318b6737cc5a737f5a7a59067aba6ae63ca
hammadsadi <hammad.sadi@yahoo.com>
2025-03-07 11:26:56 +0600

    Added Redux Persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./storage";
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      cart: cartReducer,
+      cart: persistedCartReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 };
 
