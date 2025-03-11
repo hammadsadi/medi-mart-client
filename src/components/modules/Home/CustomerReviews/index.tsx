492fe8347d5acf12cd695cd9e628b0aac77bdd04
hammadsadi <hammad.sadi@yahoo.com>
2025-03-11 11:10:54 +0600

    Shid cn Carousel Added

--- src/components/modules/Home/CustomerReviews/index.tsx
+++ src/components/modules/Home/CustomerReviews/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import MyContainer from '../../shared/MyContainer/MyContainer';
-import SectionTitle from '../../shared/SectionTitle/SectionTitle';
-import ReviewItem from './ReviewItem/ReviewItem';
+"use client";
+import React, { useEffect } from "react";
+import MyContainer from "../../shared/MyContainer/MyContainer";
+import SectionTitle from "../../shared/SectionTitle/SectionTitle";
+import ReviewItem from "./ReviewItem/ReviewItem";
+import { type CarouselApi } from "@/components/ui/carousel";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 const CustomerReviews = () => {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap());
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
   return (
     <div>
       <MyContainer>
@@ -13,14 +35,34 @@ const CustomerReviews = () => {
             sectionSubTitle="Read trusted reviews from our customers"
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-5 lg:gap-6 2xl:grid-cols-4 2xl:gap-8">
-          <ReviewItem />
-          <ReviewItem />
-          <ReviewItem />
+
+        <Carousel
+          setApi={setApi}
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <ReviewItem />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <div className="flex justify-end mt-2 gap-2">
+          <Button size="sm" onClick={() => api?.scrollTo(current - 1)}>
+            <ArrowLeft />
+          </Button>
+          <Button size="sm" onClick={() => api?.scrollTo(current + 1)}>
+            <ArrowRight />
+          </Button>
         </div>
       </MyContainer>
     </div>
   );
-}
+};
 
-export default CustomerReviews
+export default CustomerReviews;
